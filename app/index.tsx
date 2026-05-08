aa9164ac82ea8e0829aa33bacc27f1ff256b5f7e
chimolirgb <chimolilucy@gmail.com>
2026-05-08 12:48:24 +0300

    code base inspo

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "red" }}>
+      <Text style={{ color: "white", fontSize: 24 }}>HELLO JAGEDO</Text>
+    </View>
+  );
+}
